b24fbfeb83e0a7afeb0bf7fa30c114cb07340d3d
Neaptolem <Anneaptolemkob@gmail.com>
2016-03-02 22:19:18 +0200

    problem with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,29 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
 
-var files = {
-  src: ['app/app.module.js',
-  'app/app.routes.js',
-  'app/shared/navbar/navbarDirective.js',
-  'app/shared/pagetitle/pagetitleDirectice.js',
-  'app/components/home/homeModule.js',
-  'app/components/chart/chartModule.js'
-],
-  filename: 'build.js',
-  dist: 'dist'
-};
 
-gulp.task('javascript', function () {
-  return gulp.src(files.src)
-    .pipe(concat(files.filename))
-    .pipe(gulp.dest(files.dist));
+gulp.task('buildLib', function() {
+  gulp.src(require('./dependencies.json').dependencies)
+  .pipe(concat('vendor.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/js'));
 });
-
+gulp.task('jsUglify', function() {
+  return gulp.src('./app/**/*.js')
+  .pipe(concat('build.js'))
+  .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('templates', function() {
+  return gulp.src('./app/index.html')
+  .pipe(gulp.dest('./dist'));
+});
 gulp.task('clean', function () {
   return gulp.src('dist')
     .pipe(clean());
 });
 
 gulp.task('default', ['clean'], function () {
-  gulp.start('javascript');
+  gulp.start('buildLib','jsUglify','templates');
 });
