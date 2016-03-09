0f7b9b796625da51263893ea1a9879efc9bc3bd7
neaptolem <Anneaptolemkob@gmail.com>
2016-03-09 23:12:55 +0200

    fix scope

--- app/components/credit_calc/creditCalcModel.js
+++ app/components/credit_calc/creditCalcModel.js
@@ -1,9 +1,9 @@
 angular.module('app')
-  .controller('creditCalcCtrl', ['$scope', function(scope) {
-scope.result;
-scope.perMounth;
-    scope.calculate=function(calc){
-      scope.result=calc.principial*(1+(calc.term/12)*(calc.rate/100));
-      scope.perMounth=scope.result/calc.term;
+  .controller('creditCalcCtrl', ['$scope', function($scope) {
+$scope.result;
+$scope.perMounth;
+    $scope.calculate=function(calc){
+      $scope.result=calc.principial*(1+(calc.term/12)*(calc.rate/100));
+      $scope.perMounth=$scope.result/calc.term;
     }
   }]);
